7f83bdf3bebdf214498d074ae0c9553fc38b1968
mustafashaheen1 <mustafashaheen61@gmail.com>
2025-10-29 22:19:55 -0400

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,24 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imagelocations-laravel.s3.us-west-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
   },
 };
 
