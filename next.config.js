2f3b286f79e8cd13de49fc4ca2c739c3ce582e46
mustafashaheen1 <mustafashaheen61@gmail.com>
2025-11-11 23:56:12 -0500

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,19 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
         hostname: 'via.placeholder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd2tn1m6g7lc8t8.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sgs-locations-images.s3.us-west-1.amazonaws.com',
       },
       {
         protocol: 'https',
@@ -19,9 +31,14 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: '**.s3.*.amazonaws.com',
+      },
+      {
+        protocol: 'https',
         hostname: '**.supabase.co',
       },
     ],
+    unoptimized: false,
   },
 };
 
