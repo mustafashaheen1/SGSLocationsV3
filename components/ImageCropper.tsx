b4ae5f6e7962a3f25f2ca55ccb1e35adff1df2fa
mustafashaheen1 <mustafashaheen61@gmail.com>
2026-01-08 04:05:43 -0500

    cancel button text color change

--- components/ImageCropper.tsx
+++ components/ImageCropper.tsx
@@ -78,7 +78,7 @@ export default function ImageCropper({
             variant="outline"
             size="sm"
             disabled={processing}
-            className="text-white border-white hover:bg-white hover:text-black"
+            className="text-black bg-white border-white hover:bg-gray-200"
           >
             Cancel
           </Button>
