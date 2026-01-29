d1da7a75841e5c3614dc0067df4fcd3a2b039637
mustafashaheen1 <mustafashaheen61@gmail.com>
2026-01-29 12:54:35 -0500

    footer headings

--- components/footer.tsx
+++ components/footer.tsx
@@ -193,7 +193,7 @@ export function Footer() {
 
       <footer className="bg-gray-900 text-white">
         <div className="container mx-auto px-4 py-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {/* Company Info */}
           <div>
             <Link href="/" className="inline-block mb-4">
