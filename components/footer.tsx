38655d52a6bdc016ee291c399f09fb9cffae6b23
mustafashaheen1 <mustafashaheen61@gmail.com>
2026-01-30 04:54:29 -0500

    footer alignment

--- components/footer.tsx
+++ components/footer.tsx
@@ -192,7 +192,7 @@ export function Footer() {
       `}</style>
 
       <footer className="bg-gray-900 text-white">
-        <div className="container mx-auto px-4 py-12">
+        <div className="container mx-auto px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {/* Company Info */}
           <div>
